b1710867e0d483c3e8925a85609bfae9c962cf49
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-10-01 18:16:34 -0300

    Initial commit

---
+++ src/main/java/br/dev/correa/springbootproductscrud/SpringBootProductsCrudApplication.java
@@ -0,0 +1,13 @@
+package br.dev.correa.springbootproductscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductsCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProductsCrudApplication.class, args);
+    }
+
+}
