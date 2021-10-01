f8d249222b71ed7899c4fdec04244dfb25df8b98
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-10-01 18:53:00 -0300

    Update add GET all products

---
+++ src/main/java/br/dev/correa/springbootproductscrud/Product/ProductController.java
@@ -0,0 +1,26 @@
+package br.dev.correa.springbootproductscrud.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Autowired
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
