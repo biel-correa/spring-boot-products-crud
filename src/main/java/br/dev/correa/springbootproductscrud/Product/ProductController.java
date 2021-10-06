f4408a5ddb17194af402a2fb9b9934d9ccbb1185
Gabriel Corrêa <gabrielqaw@gmail.com>
2021-10-06 17:20:33 -0300

    Get product by id

--- src/main/java/br/dev/correa/springbootproductscrud/Product/ProductController.java
+++ src/main/java/br/dev/correa/springbootproductscrud/Product/ProductController.java
@@ -2,10 +2,12 @@ package br.dev.correa.springbootproductscrud.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/product")
@@ -21,5 +23,10 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
     }
 }
